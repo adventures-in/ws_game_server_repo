e9133e5ab4a913f008c7b88ccda3cc2c53418d1c
Nick Meinhold <nick.meinhold@gmail.com>
2021-06-17 21:39:09 +1000

    Add firestore via googleapis package
    - also added the onError and onDone callbacks for each websocket

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,11 +1,30 @@
 import 'dart:async';
 
 import 'package:functions_framework/functions_framework.dart';
+import 'package:googleapis/firestore/v1.dart';
+import 'package:googleapis_auth/auth_io.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_web_socket/shelf_web_socket.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
-final handler = webSocketHandler((webSocket) =>
-    webSocket.stream.listen((message) => webSocket.sink.add('$message')));
+FirestoreApi? globalFirestore;
+
+final handler = webSocketHandler(
+  (WebSocketChannel webSocket) => webSocket.stream.listen((message) {
+    webSocket.sink.add('$message');
+  }, onError: (error) {
+    print(error);
+    webSocket.sink.add(error.toString());
+  },
+      onDone: () => webSocket.sink
+          .add('DONE ${webSocket.closeCode}: ${webSocket.closeReason}')),
+);
 
 @CloudFunction()
-FutureOr<Response> function(Request request) async => handler(request);
+FutureOr<Response> function(Request request) async {
+  // Create the Firestore API client if not already created.
+  globalFirestore ??=
+      FirestoreApi(await clientViaApplicationDefaultCredentials(scopes: []));
+
+  return handler(request);
+}
